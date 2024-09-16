7c87af95e2c7480b032829411436ef52f5cc81bd
Lily Phan <79292581+pH-li@users.noreply.github.com>
2024-09-16 09:49:11 -0400

    Fixed the TODO again, forgot to change datatype

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -5,7 +5,7 @@ public class DataTypes {
 
         long s = 0;
         // below is a "foreach" loop which iterates through numbers
-        for (int x : numbers) {
+        for (long x : numbers) {
             s += x;
         }
         return s;
